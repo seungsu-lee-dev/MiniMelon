9d8baa79540628e0146729dfe6f945a947d82a43
uiop1370 <83994416+uiop1370@users.noreply.github.com>
2021-08-17 14:53:12 +0900

    MySQL 연동..

---
+++ src/main/java/com/jojoldu/book/springboot/domain/PlayListRepository.java
@@ -0,0 +1,6 @@
+package com.jojoldu.book.springboot.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayListRepository extends CrudRepository<PlayList, Long> {
+}
